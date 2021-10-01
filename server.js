e812893e7d1fa84c892b2ca9716949a5e27cf1f8
nayrannery <88459690+nayrannery@users.noreply.github.com>
2021-10-01 18:15:41 -0300

    Update server.js
    v1.1

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 
 const app = express();
-
-app.get('/',(req, res)=>{
-    res.send('Hello World!!')
+/***
+ * GET: Buscar / listar uma informação no backend
+ * POST: Criar uma informação no backend
+ * PUT: Alterar as informações no backend
+ * DELETE: Apagar informações no backend
+ */
+app.post('/users',(req, res)=>{
+    res.json({
+nome: 'Lucas',
+empresa:'UESB'
+    })
 })
 app.listen(3001);
