be8d6f7d913f6d0242b914fb780c876ec6920e7b
nayrannery <nayran.nery@hotmail.com>
2021-11-03 00:53:33 -0300

    v1.2

--- frontend/src/pages/Profile/index.js
+++ frontend/src/pages/Profile/index.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React,{useState,useEffect} from 'react';
+import "./style.css";
+import {Link, useHistory,useParams} from "react-router-dom";
+import api from '../../services/api';
+
+
+
 
 export default function Profile(){
+    const{id}= useParams();
+   const history = useHistory();
+   const initUser={
+nome:'',
+email:'',
+idade: 0,
+empresa:''
+   }
+
+   const [user,setUser] = useState(initUser);
+
+   useEffect(()=>{
+       if(id){
+           api.get(`/users/${id}`).then(response=>{
+            setUser(...response.data)
+            //console.log(response.data)
+       })
+    }
+},[]);
+ 
+
+   function onSubmit(ev){
+       ev.preventDefault();
+       const method = id ? 'put' : 'post';
+       const url = id 
+       ? `/users/${id}`
+       : '/users'
+       api[method](url,user).then((response)=>{
+           history.push('/')
+       })            
+   }
+
+   function onChange(ev){       
+    const {name,value} = ev.target;
+    setUser({...user,[name]:value})
+    console.log(user);
+    
+}
+
 return(
-    <h1>Profile</h1>
+    <div id="profile-container">
+        <h1>Cadastro</h1>
+        <form onSubmit={onSubmit}>
+            <strong>Nome</strong>
+            <input name="nome" onChange={onChange} value={user.nome}/>
+            <strong>Email</strong>
+            <input type="email" name ="email" onChange={onChange} value={user.email}/>
+            <strong>Idade</strong>
+            <input name ="idade" onChange={onChange} value={user.idade}/>
+            <strong>Empresa</strong>
+            <input name ="empresa" onChange={onChange} value={user.empresa}/> 
+
+            <div className="actions">    
+            <button className="button" type="submit">Salvar</button>      
+            <Link className="button" to="/">Voltar</Link>
+            
+            </div>
+        </form>
+    </div>
 );
 }
--- frontend/src/pages/User/index.js
+++ frontend/src/pages/User/index.js
@@ -1,40 +1,57 @@
-import React,{useState,useEffect} from 'react';
-import api from'../../services/api';
+import React, { useState, useEffect } from 'react';
+import api from '../../services/api';
+import {Link} from "react-router-dom";
 import './style.css';
 
 
-export default function User(){    
-    const [users,setUsers] = useState([]);
-    useEffect(()=>{
-        api.get('users').then(response =>{
+
+export default function User() {
+    const [users, setUsers] = useState([]);
+    useEffect(() => {
+        api.get('users').then(response => {
             setUsers(response.data);
-        }) 
+        })
 
-    },[])
-
-   
+    }, [])
     
-    return(
+    async function handleDelete(id){
+        try{
+            await api.delete(`/users/${id}`) 
+            setUsers(users.filter(user=> user.id !=id))
+        } catch(err){
+            alert('Erro ao deletar!')
+        }
+    }
 
-<div id="user-container">
-<h1>Lista de usuarios</h1>
-<ul className="user-list">
-    {users.map(user=>(        
-            <li key={user.id}>
-                <strong>Nome</strong>
-                <p>{user.name}</p>
-                <strong>email</strong>
-                <p>{user.email}</p>
-                <strong>Idade</strong>
-                <p>{user.idade}</p>
-                <strong>Empresa</strong>
-                <p>{user.empresa}</p>
-            </li>
-    ))}
-</ul>
+    return (
 
-</div>
+        <div id="user-container">            
+            <h1>Lista de usuarios</h1>
+            <Link className="button" id="create-link" to={"/create"}>Criar</Link>
+            <ul className="user-list">
+                {users.map(user => (
+                    <li key={user.id}>
+                        <strong>Nome</strong>
+                        <p>{user.nome}</p>
+                        <strong>email</strong>
+                        <p>{user.email}</p>
+                        <strong>Idade</strong>
+                        <p>{user.idade}</p>
+                        <strong>Empresa</strong>
+                        <p>{user.empresa}</p>
 
-    
+                        <div className="actions">
+                            <button className="button" onClick={()=>handleDelete(user.id)} type="button">Deletar</button>
+                            <Link className="button" to={`/update/${user.id}`}>Acessar</Link>
+                        </div>
+                    </li>
+
+
+                ))}
+            </ul>
+
+        </div>
+
+
     );
 }
